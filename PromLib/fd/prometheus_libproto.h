f8fb655c4684ef53f258271f535e3bd835233f43
mheyer <sonode@gmx.de>
2023-03-31 00:54:36 -0700

    Make it compile in gcc

---
+++ PromLib/fd/prometheus_libproto.h
@@ -0,0 +1,37 @@
+/* Automatically generated header (sfdc 1.11e)! Do not edit! */
+
+#ifndef _LIBPROTO_PROMETHEUS_H
+#define _LIBPROTO_PROMETHEUS_H
+
+#include <exec/interrupts.h>
+#include <exec/types.h>
+#include <libraries/prometheus.h>
+#include <utility/tagitem.h>
+
+#define _sfdc_strarg(a) _sfdc_strarg2(a)
+#define _sfdc_strarg2(a) #a
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _LIBPROTO_PROMETHEUS_H */
