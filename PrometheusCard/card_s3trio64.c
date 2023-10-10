c6b7280ec6e05f8f48de672d108a46883784f08a
mheyer <sonode@gmx.de>
2023-10-09 23:58:13 -0700

    Start working on Blitter support

--- PrometheusCard/card_s3trio64.c
+++ PrometheusCard/card_s3trio64.c
@@ -76,7 +76,7 @@ BOOL InitS3Trio64(struct CardBase *cb, struct BoardInfo *bi, ULONG dmaSize)
               (isTrio64Plus ? "64+" : "32/64"));
 #endif
 
-      bi->ChipRevision = ci.Revision;
+      getChipData(bi)->Revision = ci.Revision;
 
       // check for multiple hits and skip the ones already used
       //            if (current++ < count)
@@ -102,9 +102,9 @@ BOOL InitS3Trio64(struct CardBase *cb, struct BoardInfo *bi, ULONG dmaSize)
         // offsets
         bi->RegisterBase = ((UBYTE *)cb->cb_LegacyIOBase) + REGISTER_OFFSET;
         // Trio64+ places the MMIO range at BaseAddress + 0x1000000
-        bi->MemoryIOBase = (UBYTE *)((ULONG)ci.Memory0 + 0x1000000 + MMIOREGISTER_OFFSET);
+        bi->MemoryIOBase = ci.Memory0 + 0x1000000 + MMIOREGISTER_OFFSET;
         // No need to fudge with the base address here
-        bi->MemoryBase = (UBYTE *)((ULONG)ci.Memory0);
+        bi->MemoryBase = ci.Memory0;
       } else {
         if ((Prm_GetPhysicalAddress(ci.Memory0) != 0x0 ||
              ci.Memory0Size < 0x800000)) {
@@ -114,21 +114,22 @@ BOOL InitS3Trio64(struct CardBase *cb, struct BoardInfo *bi, ULONG dmaSize)
           return FALSE;
         }
         bi->RegisterBase = ((UBYTE *)cb->cb_LegacyIOBase) + REGISTER_OFFSET;
-        // This is how I understand Trio64/32 MMIO approach: 0xA8000 is
+        // This is how I understand Trio64/32 MMIO approach: 0xA0000 is
         // hardcoded as the base of the enhanced registers I need to make sure,
-        // the first 1 MB of address space don't overlap with anything. In worst
-        // case this means wasting the first 64 MB of address space...
+        // the first 1 MB of address space don't overlap with anything.
         bi->MemoryIOBase =
-            Prm_GetVirtualAddress((UBYTE *)0xA8000 + MMIOREGISTER_OFFSET);
-        // I have to push out the card's memory base address to not overlap
-        // with its own MMIO address range at 0xA8000-0xAFFFF
-        // On Trio64+ this is way easier with the "new MMIO" approach.
-        // Here we move the Linear Address Window up by 4MB. This gives us 4MB
-        // alignment and moves the LAW while not moving the PCI BAR of teh card.
-        // The assumption is that the gfx card is the first one to be initialized
-        // and thus sit at 0x00000000 in PCI address space. This way
-        // 0xA8000 is in the card's BAR and the LAW should be at 0x400000
-        bi->MemoryBase = (UBYTE *)((ULONG)ci.Memory0 + 0x400000);
+            Prm_GetVirtualAddress((UBYTE *)0xA0000 + MMIOREGISTER_OFFSET);
+
+        // I have to push out the card's Linear Address Window memory base
+        // address to not overlap with its own MMIO address range at
+        // 0xA8000-0xAFFFF On Trio64+ this is way easier with the "new MMIO"
+        // approach. Here we move the Linear Address Window up by 4MB. This
+        // gives us 4MB alignment and moves the LAW while not moving the PCI
+        // BAR of teh card. The assumption is that the gfx card is the first
+        // one to be initialized and thus sit at 0x00000000 in PCI address
+        // space. This way 0xA8000 is in the card's BAR and the LAW should be
+        // at 0x400000
+        bi->MemoryBase = ci.Memory0 + 0x400000;
       }
 
       REGBASE();
@@ -140,10 +141,6 @@ BOOL InitS3Trio64(struct CardBase *cb, struct BoardInfo *bi, ULONG dmaSize)
       InitChipL(bi);
 
       CacheClearU();
-
-      CacheClearU();
-
-
 
 #ifdef DBG
       KPrintF("prometheus.card: register server\n");
